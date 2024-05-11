a44721cc99b59b28ab7ca6eab1f8d64600d9367b
Anshor J <lordlexa10@gmail.com>
2024-05-11 10:40:46 +0700

    feat(checkout) : change can be zero

--- src/http/models/product/checkout.go
+++ src/http/models/product/checkout.go
@@ -4,7 +4,7 @@ type ProductCheckoutModel struct {
 	CustomerId     string                  `json:"customerId" binding:"required"`
 	ProductDetails []ProductCheckoutDetail `json:"productDetails" binding:"required"`
 	Paid           int                     `json:"paid" binding:"required"`
-	Change         int                     `json:"change" binding:"required"`
+	Change         int                     `json:"change"`
 }
 
 type ProductCheckoutDetail struct {
