d2b0a4a75fa3a76829e8956736587ee3811bd6e6
Anshor J <lordlexa10@gmail.com>
2024-05-12 16:23:38 +0700

    feat(checkout): allow change to zero and disallow null

--- src/http/controllers/product/checkout.go
+++ src/http/controllers/product/checkout.go
@@ -39,7 +39,7 @@ func (dbase *V1Product) ProductCheckout(c *gin.Context) {
 	for i, detail := range req.ProductDetails {
 		_, err := uuid.Parse(detail.ProductId)
 		if err != nil {
-			c.JSON(http.StatusNotFound, gin.H{"error": "Wrong product id"})
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Wrong product id"})
 			return
 		}
 
@@ -115,7 +115,7 @@ func (dbase *V1Product) ProductCheckout(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "Paid not enough"})
 		return
 	}
-	if req.Change != req.Paid-productTotalPrice || req.Change < 0 {
+	if *req.Change != req.Paid-productTotalPrice || *req.Change < 0 {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "Change is wrong"})
 		return
 	}
